f96fc9660275d5450f29ffe97fa7abcbc5d74678
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2022-10-07 15:06:18 +0200

    Add customizable window name

--- components/Frame.tsx
+++ components/Frame.tsx
@@ -3,7 +3,7 @@ import { useAtom } from "jotai";
 import React, { useContext } from "react";
 
 import styles from "styles/Frame.module.css";
-import { showBackgroundAtom } from "../store";
+import { fileNameAtom, showBackgroundAtom } from "../store";
 import { FrameContext } from "../store/FrameContextStore";
 import { paddingAtom } from "../store/padding";
 import Editor from "./Editor";
@@ -14,6 +14,7 @@ const Frame: React.FC = () => {
   const frameContext = useContext(FrameContext);
   const [padding] = useAtom(paddingAtom);
   const [showBackground] = useAtom(showBackgroundAtom);
+  const [fileName, setFileName] = useAtom(fileNameAtom);
 
   return (
     <div className={styles.frameContainer}>
@@ -37,7 +38,15 @@ const Frame: React.FC = () => {
                 <div className={styles.control}></div>
                 <div className={styles.control}></div>
               </div>
-              <div className={styles.fileName}>Untitled-1</div>
+              <div className={styles.fileName}>
+                <input
+                  type="text"
+                  value={fileName}
+                  onChange={(event) => setFileName(event.target.value)}
+                  spellCheck={false}
+                />
+                {fileName.length === 0 ? <span>Untitled-1</span> : null}
+              </div>
             </div>
             <Editor />
           </div>
--- store/index.ts
+++ store/index.ts
@@ -2,3 +2,4 @@ import { atom } from "jotai";
 
 export const windowWidthAtom = atom<number | null>(null);
 export const showBackgroundAtom = atom<boolean>(true);
+export const fileNameAtom = atom<string>("");
