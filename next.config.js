63564c5861ab1751c6755f4e9c788065ffc00f07
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2022-10-07 10:43:24 +0200

    Add resizing

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 };
 
 module.exports = nextConfig;
