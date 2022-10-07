cb7fd2a3bf746bd8ee105b8b960329a5eff344d2
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2022-10-07 14:17:19 +0200

    Small fixes for padding control

--- store/padding.ts
+++ store/padding.ts
@@ -4,4 +4,4 @@ export const PADDING_OPTIONS = [16, 32, 64, 128] as const;
 
 export type Padding = typeof PADDING_OPTIONS[number];
 
-export const paddingAtom = atom<Padding>(PADDING_OPTIONS[1]);
+export const paddingAtom = atom<Padding>(PADDING_OPTIONS[2]);
