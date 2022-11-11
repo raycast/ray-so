4286d2304f0591c8dff821de6f5694da9cc74acc
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2022-11-11 10:52:08 +0100

    Move LanguageSelect to use Radix

--- components/LanguageControl.tsx
+++ components/LanguageControl.tsx
@@ -1,11 +1,13 @@
 import { useAtom } from "jotai";
 import React from "react";
+import * as Select from "@radix-ui/react-select";
 import { autoDetectLanguageAtom, selectedLanguageAtom } from "../store/code";
 import ControlContainer from "./ControlContainer";
-import FilterableSelect from "./FilterableSelect";
-import { LANGUAGES } from "../util/languages";
+import { Language, LANGUAGES } from "../util/languages";
 
-import styles from "styles/LanguageControl.module.css";
+import ChevronUpIcon from "assets/icons/chevron-up-16.svg";
+
+import styles from "styles/Select.module.css";
 
 const LanguageControl: React.FC = () => {
   const [selectedLanguage, setSelectedLanguage] = useAtom(selectedLanguageAtom);
@@ -13,28 +15,58 @@ const LanguageControl: React.FC = () => {
 
   return (
     <ControlContainer title="Language">
-      <FilterableSelect
-        className={styles.languageSelect}
-        labelFormatter={(label) =>
-          `${label}${autoDetectLanguage ? " (auto)" : ""}`
-        }
-        items={[
-          {
-            value: "Auto-Detect",
-            selected: selectedLanguage === null,
-            onSelect() {
-              setSelectedLanguage(null);
-            },
-          },
-          ...Object.values(LANGUAGES).map((language) => ({
-            value: language.name,
-            selected: language === selectedLanguage,
-            onSelect() {
-              setSelectedLanguage(language);
-            },
-          })),
-        ]}
-      />
+      <Select.Root
+        value={selectedLanguage?.name || "auto-detect"}
+        onValueChange={(value) => {
+          if (value === "auto-detect") {
+            setSelectedLanguage(null);
+          } else {
+            setSelectedLanguage(
+              Object.values(LANGUAGES).find((l) => l.name === value) as Language
+            );
+          }
+        }}
+      >
+        <Select.Trigger className={styles.trigger}>
+          <Select.Value>
+            <span
+              style={{
+                display: "block",
+                textAlign: "start",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                whiteSpace: "nowrap",
+                width: "100px",
+              }}
+            >
+              {selectedLanguage?.name || "auto-detect"}
+              {autoDetectLanguage ? " (auto)" : ""}
+            </span>
+          </Select.Value>
+          <Select.Icon className={styles.icon}>
+            <ChevronUpIcon />
+          </Select.Icon>
+        </Select.Trigger>
+
+        <Select.Portal>
+          <Select.Content className={styles.content}>
+            <Select.Viewport className={styles.viewport}>
+              <Select.Item value="auto-detect" className={styles.item}>
+                <Select.SelectItemText>Auto-Detect</Select.SelectItemText>
+              </Select.Item>
+              {Object.values(LANGUAGES).map((language, index) => (
+                <Select.Item
+                  key={index}
+                  value={language.name}
+                  className={styles.item}
+                >
+                  <Select.SelectItemText>{language.name}</Select.SelectItemText>
+                </Select.Item>
+              ))}
+            </Select.Viewport>
+          </Select.Content>
+        </Select.Portal>
+      </Select.Root>
     </ControlContainer>
   );
 };
