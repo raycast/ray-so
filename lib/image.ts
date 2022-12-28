50476a8b72d3950a795256db912f9fa3b76947a0
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2022-12-28 15:36:36 +0100

    Remove pixelRatio logic
    This is actually handled natively by html-to-image and was resulting in
    images that were 4x the original size instead of 2x

--- lib/image.ts
+++ lib/image.ts
@@ -3,40 +3,18 @@ import {
   toSvg as htmlToSvg,
   toBlob as htmlToBlob,
 } from "html-to-image";
-import { changeDpiDataUrl } from "changedpi";
 
 const imageFilter = (node: HTMLElement) =>
   node.tagName !== "TEXTAREA" && !node.dataset?.ignoreInExport;
 
-const scale = 2;
-const dpi = 72;
-
 export const toPng = async (node: HTMLElement) => {
-  const dataUrl = await htmlToPng(node, {
-    height: node.offsetHeight * scale,
-    width: node.offsetWidth * scale,
-    style: {
-      transform: `scale(${scale})`,
-      transformOrigin: `top left`,
-      width: `${node.offsetWidth}px`,
-      height: `${node.offsetHeight}px`,
-    },
+  return htmlToPng(node, {
     filter: imageFilter,
   });
-
-  return changeDpiDataUrl(dataUrl, dpi * scale);
 };
 
 export const toBlob = async (node: HTMLElement) => {
   return htmlToBlob(node, {
-    height: node.offsetHeight * scale,
-    width: node.offsetWidth * scale,
-    style: {
-      transform: `scale(${scale})`,
-      transformOrigin: `top left`,
-      width: `${node.offsetWidth}px`,
-      height: `${node.offsetHeight}px`,
-    },
     filter: imageFilter,
   });
 };
