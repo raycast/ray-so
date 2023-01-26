c0646cf5ec2ea50de2296c7ea34020dede3b219c
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2023-01-26 15:17:35 +0100

    Fix fileName setting
    The previous fixed setting it through the location.hash, but broke
    setting it through the UI :sweat_smile:

--- store/index.ts
+++ store/index.ts
@@ -10,6 +10,9 @@ export const showBackgroundAtom = atomWithHash<boolean>("background", true, {
 
 export const fileNameAtom = atomWithHash<string>("title", "", {
   delayInit: true,
+  serialize(val) {
+    return val;
+  },
   deserialize(str) {
     return `${str}`;
   },
