874b35456bbc2a6aee772c29e302bab813cd6f2e
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2023-01-28 10:52:29 +0100

    Set viewport width to 860
    Instead of making the whole app mobile-ready I figured I'd start with
    just rendering correctly on mobile.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=860, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
