8c94b3387ca4d591f805635a33c7214efe76c8b0
Pedro Duarte <oi@ped.ro>
2024-02-05 14:23:30 +0100

    move API to edge

---
+++ app/api/shorten-url/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import Dub from "dub";
+
+export const runtime = "edge";
+
+const dub = new Dub({
+  token: "0e7uX8DwmzNxmRalhRculkIh",
+  projectSlug: "ray-so",
+});
+
+export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams;
+  const urlQuery = searchParams.get("url");
+
+  const url = new URL(urlQuery as string);
+
+  if (!url) {
+    return NextResponse.json({ error: "Missing URL" });
+  }
+
+  if (url.hostname === "ray.so" || url.hostname.includes("raycastapp.vercel.app") || url.hostname === "localhost") {
+    const link = await dub.links.create({
+      url: url.href,
+      domain: "go.ray.so",
+    });
+    return NextResponse.json({ link: `https://ray.so/${link.key}` });
+  }
+
+  return NextResponse.json({ error: "Unable to shorten this link" }, { status: 400 });
+}
