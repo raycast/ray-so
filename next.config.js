358b22aa92a0ced3b227181090e769882cb26c03
Pedro Duarte <oi@ped.ro>
2024-02-09 11:44:25 +0100

    Set up Cors in shorten url API

--- next.config.js
+++ next.config.js
@@ -20,6 +20,24 @@ const nextConfig = {
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/shorten-url",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*.ray.so" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
