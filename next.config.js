91f834ce6f7bd4fbf023affb7d5cf8d0a0bf4d50
Pedro Duarte <oi@ped.ro>
2024-02-09 12:03:25 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -27,7 +27,7 @@ const nextConfig = {
         source: "/api/shorten-url",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*.ray.so" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
           {
             key: "Access-Control-Allow-Headers",
