b29d479831b0cded5c948414a67f41903431ad39
Samuel Kraft <samuelkraft@me.com>
2024-04-15 20:44:12 +0200

    use shiki for syntax highlighting

--- components/HighlightedCode.tsx
+++ components/HighlightedCode.tsx
@@ -1,9 +1,11 @@
 import classNames from "classnames";
-import hljs from "highlight.js";
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Language, LANGUAGES } from "../util/languages";
 
 import styles from "../styles/Editor.module.css";
+import { Highlighter, getHighlighter } from "shiki";
+import { useAtom } from "jotai";
+import { themeAtom } from "../store/themes";
 
 type PropTypes = {
   selectedLanguage: Language | null;
@@ -11,15 +13,31 @@ type PropTypes = {
 };
 
 const HighlightedCode: React.FC<PropTypes> = ({ selectedLanguage, code }) => {
+  const [highlighter, setHighlighter] = useState<Highlighter | null>(null);
+  const [theme] = useAtom(themeAtom);
+
+  useEffect(() => {
+    getHighlighter({
+      themes: ["nord", "monokai", "github-dark", "github-light", "solarized-dark", "solarized-light"],
+      langs: ["javascript", "typescript", "swift", "html", "r", "rust", "python", "go", "java"],
+    }).then((highlighter) => {
+      setHighlighter(highlighter);
+    });
+  }, [theme]);
+
   const html = useMemo(() => {
     if (selectedLanguage && selectedLanguage !== LANGUAGES.plaintext) {
-      return hljs.highlight(code, { language: selectedLanguage.className }).value;
+      const result = highlighter?.codeToHtml(code, {
+        lang: "javascript",
+        theme: "nord",
+      });
+      return result;
     } else {
       return code.replace(/[\u00A0-\u9999<>\&]/g, function (i) {
         return `&#${i.charCodeAt(0)};`;
       });
     }
-  }, [code, selectedLanguage]);
+  }, [code, selectedLanguage, highlighter]);
 
   const preView = useMemo(
     () => (
