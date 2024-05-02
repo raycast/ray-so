ac0bee9d2f7fde9aaec9aef26e07137656379b03
Samuel Kraft <samuelkraft@me.com>
2024-05-02 10:47:44 +0200

    Highlight lines

--- store/index.ts
+++ store/index.ts
@@ -23,3 +23,13 @@ export const fileNameAtom = atomWithHash<string>("title", "", {
 export const highlighterAtom = atom<Highlighter | null>(null);
 
 export const loadingLanguageAtom = atom<boolean>(false);
+
+export const highlightedLinesAtom = atomWithHash<number[]>("highlightedLines", [1, 2, 3], {
+  delayInit: true,
+  serialize(val) {
+    return val.join(",");
+  },
+  deserialize(str) {
+    return str ? str.split(",").map(Number) : [];
+  },
+});
