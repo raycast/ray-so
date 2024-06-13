7eded9e1bab803cb2a2db64e3da6f89884923a5f
Samuel Kraft <samuelkraft@me.com>
2024-06-13 20:56:14 +0200

    Update page.tsx

--- app/(navigation)/icon/page.tsx
+++ app/(navigation)/icon/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { IconGenerator } from "./icon-generator";
+import { Suspense } from "react";
 
 const pageTitle = "Icon Maker by Raycast";
 const pageDescription = "Create beautiful icons for your next project, app, or Raycast extension.";
@@ -14,5 +15,9 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-  return <IconGenerator />;
+  return (
+    <Suspense>
+      <IconGenerator />
+    </Suspense>
+  );
 }
