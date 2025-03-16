c83d4331ca8b119adf302deba75642cbf9f7b952
Kian <kian.pasani@gmail.com>
2025-03-16 16:11:09 +0100

    Update prompt.ts: added copy-currently-playing-spotify-to-apple

--- app/(navigation)/prompts/prompts.ts
+++ app/(navigation)/prompts/prompts.ts
@@ -516,6 +516,15 @@ const music: Prompt[] = [
     model: "raycast-ray1-mini",
     date: "2025-03-16",
   },
+  {
+    id: "copy-currently-playing-spotify-to-apple",
+    title: "Copy Apple Music URL from Currently Playing Spotify Track",
+    prompt: "@spotify-player{id=320f40ef-a633-415a-ab0e-1e99515478f7} Take the currently playing track, @music{id=2bfe94cd-50ab-4e48-a6cd-f1ff47a72706} search that track and @clipboard{id=builtin_package_clipboardHistory} copy the link of the spotify song. Not in markdown, just the link.",
+    icon: "music",
+    creativity: "none",
+    model: "raycast-ray1-mini",
+    date: "2025-03-16",
+  }
 ];
 
 const ideas: Prompt[] = [
