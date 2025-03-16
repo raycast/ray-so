3fb0b12d1378be73860bb10af9aeac9dffc31071
Andreas Elia <andreaselia@live.co.uk>
2025-03-16 19:14:48 +0000

    update import url

--- app/(navigation)/themes/components/redirect-to-raycast.tsx
+++ app/(navigation)/themes/components/redirect-to-raycast.tsx
@@ -5,8 +5,12 @@ import { makeRaycastImportUrl } from "@themes/lib/url";
 
 export function RedirectToRaycast({ theme }: { theme: Theme }) {
   React.useEffect(() => {
-    console.log("Opening theme in Raycast from redirect");
-    window.open(makeRaycastImportUrl(theme));
+    async function openImportUrl() {
+      console.log("Opening theme in Raycast from redirect");
+      const importUrl = await makeRaycastImportUrl(theme);
+      window.open(importUrl);
+    }
+    openImportUrl();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return null;
