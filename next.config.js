35e78004e080f1d20c8382035556ec5be1d904fa
Tim Neutkens <tim@timneutkens.nl>
2026-01-16 12:59:02 +0100

    Remove webpack config as it's no longer used

--- next.config.js
+++ next.config.js
@@ -52,51 +52,6 @@ const nextConfig = {
       },
     },
   },
-  webpack(config) {
-    // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.(".svg"));
-
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: fileLoaderRule.issuer,
-      resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-      use: [
-        {
-          loader: "@svgr/webpack",
-          options: {
-            svgoConfig: {
-              plugins: [
-                {
-                  name: "removeViewBox",
-                  active: false,
-                },
-              ],
-            },
-          },
-        },
-      ],
-    });
-
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
-    fileLoaderRule.exclude = /\.svg$/i;
-
-    // find the built-in loader
-    const imageLoaderRule = config.module.rules.find((rule) => rule.loader === "next-image-loader");
-    // make the loader ignore *.inline files
-    imageLoaderRule.exclude = /\.inline\.(png|jpg|svg)$/i;
-
-    // add a new URL loader for *.inline files
-    config.module.rules.push({
-      test: /\.inline\.(png|jpg|gif)$/i,
-      use: [
-        {
-          loader: "url-loader",
-        },
-      ],
-    });
-
-    return config;
-  },
   async rewrites() {
     return {
       fallback: [
